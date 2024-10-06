83e1665bb010eca56256bb313f595dd5fba9156f
GaneshGhatti <ghattiganesh8@gmail.com>
2024-10-06 14:34:47 +0530

    block component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
 import "./Home.scss";
+import BlockComponent from "./components/BlockComponent";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      
+      <BlockComponent />
     </>
   );
 }
